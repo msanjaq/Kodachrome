b311263c149198d1918acb21d04a828fcf3a0fd8
msanjaq <msanjaq@uci.edu>
2017-08-04 00:24:32 -0700

    Changed the game from fullscreen to a fix screen length. Now shows Player.
    Realized that making this game fullscreen would cause a lot  more complexity to the level design stage, since I would have to account for different size screens as well.

--- view.py
+++ view.py
@@ -6,22 +6,20 @@ class View(object):
     '''
 
 
-    def __init__(self, model):
+    def __init__(self, model, height=800, width=400):
         '''
         Constructor
         '''
         self.model  = model
-        self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        self.screen = pygame.display.set_mode([height,width])
+        print(type(self.screen))
     
     def run(self):
         clock = pygame.time.Clock()
         
         while self.model.update():
+            self.screen.fill((0,0,0))
+            for sprite in self.model.sprite_group:
+                self.screen.blit(sprite.image, sprite.rect.topleft)
             clock.tick(60)
             pygame.display.update()
-
-if __name__ == "__main__":
-    import model
-    import controller
-    v = View(model.Model(controller.Controller()))
-    v.run()
