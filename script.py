ac9f24b402606615d955595261f1a4bc8fa902e0
msanjaq <msanjaq@uci.edu>
2017-08-04 00:25:29 -0700

    Running this gives you an empty void where you can move the character left and right.

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 from controller import Controller
 from model      import Model
 from view       import View
+from player     import Player
 
 if __name__ == '__main__':
-    v = View(Model(Controller()))
+    v = View(Model(Controller(Player())))
     v.run()
     pass
