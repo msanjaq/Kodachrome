021028bc25d78849fd2b25fac677af30bf1b55b7
msanjaq <msanjaq@uci.edu>
2017-08-04 00:16:36 -0700

    Added controlls to move player

--- controller.py
+++ controller.py
@@ -6,15 +6,25 @@ class Controller(object):
     '''
 
 
-    def __init__(self):
+    def __init__(self, player):
         '''
         Constructor
         '''
-        self.quit = False
+        self.quit   = False
+        self.player = player
         
         
     def get(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    self.player.left()
+
+                if event.key == pygame.K_RIGHT:
+                    self.player.right()
+
                 if event.key == pygame.K_ESCAPE:
                     self.quit = True
+            
+            elif event.type == pygame.KEYUP:
+                self.player.stop()
