e62c47423db1fbe357b8a4b48f3fd9ae50ac18a9
msanjaq <msanjaq@uci.edu>
2017-08-04 20:56:18 -0700

    Added collision logic for player, gravity, and fixed some control bugs
    Collison logic is for platforms only.  There was a bug that switching between left() and right() caused the Player to stop moving all together. I split the stop() function into two different functions stop_right() and stop_left() to fix this.

---
+++ sprites/player.py
@@ -0,0 +1,79 @@
+'''
+Created on Aug 3, 2017
+
+@author: Moe Sanjaq
+'''
+import pygame
+from .platform import Platform
+
+
+class Player(pygame.sprite.Sprite):
+    '''
+    classdocs
+    '''
+
+    speed = 10
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        pygame.sprite.Sprite.__init__(self)
+
+        self.image    = pygame.Surface((10,30))
+        self.image.fill((255,255,255))
+
+        self.rect   = self.image.get_rect()
+        self.rect.x = 30
+        self.rect.y = 30
+
+        self.dx = 0
+        self.dy = 0
+        
+    def jump(self):
+        self.dy = -20
+    
+    def left(self):
+        self.dx = -Player.speed
+        
+    def right(self):
+        self.dx = +Player.speed
+    
+    def stop_left(self):
+        if self.dx == -Player.speed:
+            self.dx = 0
+    
+    def stop_right(self):
+        if self.dx == Player.speed:
+            self.dx = 0
+    
+    def update(self):
+        self._calc_gravity()
+
+        self.rect.x += self.dx
+    
+        self.rect.y += self.dy
+        for sprite in self._get_collisions():
+            if isinstance(sprite, Platform):
+                if self.dy > 0:
+                    self.rect.bottom = sprite.rect.top
+                
+                elif self.dy < 0:
+                    self.rect.top = sprite.rect.bottom
+                
+                self.dy = 0
+                
+        
+    
+    def _calc_gravity(self):
+        self.dy += 1
+
+        
+    def _get_collisions(self):
+        for group in self.groups():
+            collisions = pygame.sprite.spritecollide(self, group, False)
+            return collisions
+        
+    
+        
+    
+        
