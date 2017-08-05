19ee08c1e10b1655cd9b1840dbf95da05469db41
msanjaq <msanjaq@uci.edu>
2017-08-05 12:32:55 -0700

    Witdth and Height are now static variables.

--- view.py
+++ view.py
@@ -6,18 +6,20 @@ class View(object):
     '''
 
 
-    def __init__(self, model, height=800, width=400):
+    width  = 800
+    height = 800
+    def __init__(self, model):
         '''
         Constructor
         '''
         self.model  = model
-        self.screen = pygame.display.set_mode([height,width])
-        print(type(self.screen))
+        self.screen = pygame.display.set_mode([View.width, View.height])
     
     def run(self):
         clock = pygame.time.Clock()
         
-        while self.model.update():
+        while True:
+            self.model.update()
             self.screen.fill((0,0,0))
             for sprite in self.model.sprite_group:
                 self.screen.blit(sprite.image, sprite.rect.topleft)
