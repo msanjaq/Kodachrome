59d9c5447d42c2e4e15342a5ee757bb5e9842890
msanjaq <msanjaq@uci.edu>
2017-08-05 19:14:00 -0700

    Fixed scaling issue

--- level_generator.py
+++ level_generator.py
@@ -19,18 +19,25 @@ class Level(object):
         Constructor
         '''
         self.platform_list = []
-        self.player        = Player(player_start_coord)
+        #self.player        = Player(player_start_coord)
+        self.player = None
         
         with open(level_file) as file:
             self._level = file.readlines()
         
         self._width_scale  = level_width  / _get_longest_line_len(self._level)
+        print(_get_longest_line_len(self._level))
         self._height_scale = level_height / len(self._level)
         
         for line_num in range(len(self._level)):
             for col_num in range(len(self._level[line_num])):
                 if self._level[line_num][col_num] == "P":
                     self._add_platform(line_num, col_num)
+                
+                elif self._level[line_num][col_num] == "p":
+                    x = col_num   * self._width_scale 
+                    y = line_num  * self._height_scale
+                    self.player = Player((x,y))
         
         
     
@@ -40,12 +47,14 @@ class Level(object):
         text_width  = _get_platform_width(self._level[line_num][col_num+1:], line_num, col_num)
         text_height = _get_platform_height(self._level[line_num+1:], line_num, col_num)
         width       = self._width_scale  * text_width
-        height      = self._height_scale * text_height 
+        height      = self._height_scale * text_height
+        print("scale: ",self._width_scale, self._height_scale)
+        print(x,y,width,height)
         self.platform_list.append(Platform(x, y, width, height))
     
 
 def _get_platform_width(line: str, line_num: int, col_num: int) -> int:
-    result = 1
+    result = 2
     for c in line:
         if c == "E":
             return result
@@ -58,7 +67,7 @@ def _get_platform_width(line: str, line_num: int, col_num: int) -> int:
              
 
 def _get_platform_height(lines: [str], line_num: int, col_num: int) -> int:
-    result = 1
+    result = 2
     for line in lines:
         if len(line) > col_num and line[col_num] == "E":
             return result
@@ -71,7 +80,7 @@ def _get_platform_height(lines: [str], line_num: int, col_num: int) -> int:
     
 
 def _get_longest_line_len(lines: [str]) -> int:
-    return len(max(lines, key=lambda line : len(line)))
+    return len(max(lines, key=lambda line : len(line)).strip())
         
 
 if __name__ == "__main__":
